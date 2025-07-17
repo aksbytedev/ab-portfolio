b779696e08c299f29e97c399f83eb4bf4b68f3cb
aksbytedev <aksbytedev@users.noreply.github.com>
2025-07-17 11:43:31 +0000

    deploy: aksbytedev/akshya-portfolio@4bb2e1611d5bb1215c5fcf1e1f1adeab521957b2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon/web-app-manifest-192x192.png": "20888b3b598d17c7d82da1d70cd57861",
 "favicon/favicon.svg": "5f1547c8a37b4508544dd2dcd41b43d3",
 "manifest.json": "08c61165b0cae13660db548c72e1e96e",
-"flutter_bootstrap.js": "b7dfd863d5f8b0807cc15ffd5621cb94",
+"flutter_bootstrap.js": "a772065789235cafc782181b2130c89b",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"main.dart.js": "e996e1dbc029c60daa808923b7239690"};
+"main.dart.js": "547ae89a6614ab4af374f318a1d123cc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
