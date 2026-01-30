e93933544cf10a06b79364894ef623ca58ae97b1
aksbytedev <aksbytedev@users.noreply.github.com>
2026-01-30 12:39:08 +0000

    deploy: aksbytedev/akshya-portfolio@99a83aec69f3d64e263696e2c8f390a22dd043ff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2708990341"
+    serviceWorkerVersion: "3901531640"
   }
 });
